5e3d1b1df37c50ee613a86b100db7b21981b153b
jhleeon <jhleon288@gmail.com>
2024-01-04 02:07:34 +0600

    laravel project:user management system-install fortify,config bootstrap using vite-status: wip

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <title>{{ config('app.name', 'User Management System') }}</title>
+        @vite('resources/sass/app.scss')
+    </head>
+    <body>
+
+        @vite('resources/js/app.js')
+    </body>
+</html>
